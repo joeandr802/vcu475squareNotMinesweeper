a3615c67aa369dc63dfe600cc8d33a557f29181c
joeandr802 <kearneyja@vcu.edu>
2022-02-24 18:27:51 -0500

    Coded play button

--- DotsandBoxes/app/src/main/java/com/example/dotsandboxes/MainActivity.java
+++ DotsandBoxes/app/src/main/java/com/example/dotsandboxes/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.dotsandboxes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    public Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.playButton);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Play.class);
+                startActivity(intent);
+            }
+        });
     }
 }
