3c4b40ed0998f803d41873ab4a60f7d9c0c9f8db
joeandr802 <kearneyja@vcu.edu>
2022-02-27 15:50:36 -0500

    Minor changes to address suggested problems

--- DotsandBoxes/app/src/main/java/com/example/dotsandboxes/MainActivity.java
+++ DotsandBoxes/app/src/main/java/com/example/dotsandboxes/MainActivity.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,14 +14,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = (Button) findViewById(R.id.playButton);
+        button = findViewById(R.id.playButton);
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, Play.class);
-                startActivity(intent);
-            }
+        button.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, Play.class);
+            startActivity(intent);
         });
     }
 }
