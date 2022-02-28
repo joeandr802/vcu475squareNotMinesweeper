c810b1ce3424f1da95209ba3984dba3737fc36e7
HalterFE <halterje@vcu.edu>
2022-02-28 02:04:26 -0500

    Update Backbone.java
    Added important variables to be used in the class later.

--- DotsandBoxes/app/src/main/java/com/example/dotsandboxes/Backbone.java
+++ DotsandBoxes/app/src/main/java/com/example/dotsandboxes/Backbone.java
@@ -1,6 +1,13 @@
 package com.example.dotsandboxes;
 
 public class Backbone {
+    int p1points, p2points; //keeps tally of points for the two players
+    int sizeBoard = 5; // size of the board
+    int numMoves = 40; //how many moves can be made before the round ends
+    boolean currTurn = true; //true if p1's turn, false if p2's
+    String[][] dotData = new String[sizeBoard][sizeBoard]; //keeps data of board stored
+    //important variables for use later
+
     public static void main(String[] args){
         clearBoard(); //sets app to be used later
         mainGame(); //runs the actual game
