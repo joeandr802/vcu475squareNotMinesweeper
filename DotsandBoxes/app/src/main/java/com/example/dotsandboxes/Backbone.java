89798b8988957fb1a5d15501b06f5d6864da690a
HalterFE <halterje@vcu.edu>
2022-02-28 02:17:44 -0500

    Update Backbone.java
    Added the main game method that will allow game to be played.

--- DotsandBoxes/app/src/main/java/com/example/dotsandboxes/Backbone.java
+++ DotsandBoxes/app/src/main/java/com/example/dotsandboxes/Backbone.java
@@ -17,6 +17,23 @@ public class Backbone {
     }
     //clears the board so it can be used for repeat games
 
+    public void mainGame(){
+        int[][] dot1 = new int[sizeBoard][sizeBoard];
+        int[][] dot2 = new int[sizeBoard][sizeBoard];
+        //dots the user selects
+
+        while(numMoves > 0){
+            dotsConnected(dot1, dot2); //takes two connected dots
+            if(!isConnectPoints(dot1, dot2)){
+                currTurn = !currTurn;
+            }
+            //flips player's turn if no box was filled
+
+            numMoves--;// subtracts moves to be made by 1
+        }//while moves remain to be played
+    }
+    //game itself
+
     public static void main(String[] args){
         Backbone back = new Backbone();
         back.clearBoard(); //sets app to be used later
