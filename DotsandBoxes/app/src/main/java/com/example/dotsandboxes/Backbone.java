281a82d30bc1b8f6f4ea44c76887d29264dd9a28
HalterFE <halterje@vcu.edu>
2022-02-28 01:59:41 -0500

    Added new class for backbone
    Added a new class that will act as the backbone for the dots and boxes game as well as a general main method.

---
+++ DotsandBoxes/app/src/main/java/com/example/dotsandboxes/Backbone.java
@@ -0,0 +1,9 @@
+package com.example.dotsandboxes;
+
+public class Backbone {
+    public static void main(String[] args){
+        clearBoard(); //sets app to be used later
+        mainGame(); //runs the actual game
+        results(); //displays the results
+    }
+}
