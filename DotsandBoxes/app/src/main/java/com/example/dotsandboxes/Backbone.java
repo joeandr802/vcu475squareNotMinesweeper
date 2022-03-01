427679ae70842fa3b972c71f7e01edb798781f8b
HalterFE <halterje@vcu.edu>
2022-03-01 15:35:38 -0500

    Update Backbone.java
    Added functionality to take in x and y coords of both dots chosen.

--- DotsandBoxes/app/src/main/java/com/example/dotsandboxes/Backbone.java
+++ DotsandBoxes/app/src/main/java/com/example/dotsandboxes/Backbone.java
@@ -2,6 +2,7 @@ package com.example.dotsandboxes;
 
 public class Backbone {
     int p1points, p2points; //keeps tally of points for the two players
+    int x1, x2, y1, y2; //x and y coords of 2 dots chosen
     int sizeBoard = 5; // size of the board
     int numMoves = 40; //how many moves can be made before the round ends
     boolean currTurn = true; //true if p1's turn, false if p2's
@@ -16,10 +17,20 @@ public class Backbone {
         }
     }
     //clears the board so it can be used for repeat games
-    
+
+    public boolean isConnectPoints(int[][] dot1, int[][] dot2){
+        int pointsMade = 0; //home many points were made this turn
+        boolean result = false;
+    }
+    //lets program know if a box was filled in
+
     public void dotsConnected(int[][] dot1, int[][] dot2){
         self.dot1 = dot1;
         self.dot2 = dot2;
+        self.x1 = x1;
+        self.x2 = x2;
+        self.y1 = y1;
+        self.y2 = y2;
     }
     //adds functionality to connect to main game about what is played
 
