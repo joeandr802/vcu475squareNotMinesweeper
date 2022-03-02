b5445f648b3e1f8f9dcf0fab8f367cfecb858bfb
lopezboje <98184776+lopezboje@users.noreply.github.com>
2022-03-02 14:12:24 -0500

    Added Line images
    Margins wasn't going to work.
    
    Instead use line assets and turn invisible. When line is made, turn visible.
    
    Need to add rows for vertical lines

--- DotsandBoxes/app/src/main/java/com/example/dotsandboxes/MainActivity.java
+++ DotsandBoxes/app/src/main/java/com/example/dotsandboxes/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(v -> {
             Intent intent = new Intent(MainActivity.this, Play.class);
             startActivity(intent);
+            System.out.println("test");
         });
     }
 }
--- DotsandBoxes/app/src/main/java/com/example/dotsandboxes/Play.java
+++ DotsandBoxes/app/src/main/java/com/example/dotsandboxes/Play.java
@@ -1,15 +1,26 @@
 package com.example.dotsandboxes;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Play extends AppCompatActivity {
+    public int[] cordsA = new int[2];
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
 
+
+    }
+
+    public void getCord(View view){
+        char[] contentDes = view.getContentDescription().toString().toCharArray();
+
+        for(int i = 0; i < 2; i++)
+            cordsA[i] = contentDes[i]-48;
     }
 }
