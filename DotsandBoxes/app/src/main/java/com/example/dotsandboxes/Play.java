21574564f89ee8bd96d67a4253b1923ee2fc2efb
joeandr802 <kearneyja@vcu.edu>
2022-03-02 22:57:07 -0500

    Wrote map IDs

--- DotsandBoxes/app/src/main/java/com/example/dotsandboxes/Play.java
+++ DotsandBoxes/app/src/main/java/com/example/dotsandboxes/Play.java
@@ -73,6 +73,42 @@ public class Play extends AppCompatActivity {
         mapViews.put("0102", (ImageView)findViewById(R.id.h0102));
         mapViews.put("0203", (ImageView)findViewById(R.id.h0203));
         mapViews.put("0304", (ImageView)findViewById(R.id.h0304));
+        mapViews.put("0010", (ImageView)findViewById(R.id.v0010));
+        mapViews.put("0111", (ImageView)findViewById(R.id.v0111));
+        mapViews.put("0212", (ImageView)findViewById(R.id.v0212));
+        mapViews.put("0313", (ImageView)findViewById(R.id.v0313));
+        mapViews.put("0414", (ImageView)findViewById(R.id.v0414));
+        mapViews.put("1011", (ImageView)findViewById(R.id.h1011));
+        mapViews.put("1112", (ImageView)findViewById(R.id.h1112));
+        mapViews.put("1213", (ImageView)findViewById(R.id.h1213));
+        mapViews.put("1314", (ImageView)findViewById(R.id.h1314));
+        mapViews.put("1020", (ImageView)findViewById(R.id.v1020));
+        mapViews.put("1121", (ImageView)findViewById(R.id.v1121));
+        mapViews.put("1222", (ImageView)findViewById(R.id.v1222));
+        mapViews.put("1323", (ImageView)findViewById(R.id.v1323));
+        mapViews.put("1424", (ImageView)findViewById(R.id.v1424));
+        mapViews.put("2021", (ImageView)findViewById(R.id.h2021));
+        mapViews.put("2122", (ImageView)findViewById(R.id.h2122));
+        mapViews.put("2223", (ImageView)findViewById(R.id.h2223));
+        mapViews.put("2324", (ImageView)findViewById(R.id.h2324));
+        mapViews.put("2030", (ImageView)findViewById(R.id.v2030));
+        mapViews.put("2131", (ImageView)findViewById(R.id.v2131));
+        mapViews.put("2232", (ImageView)findViewById(R.id.v2232));
+        mapViews.put("2333", (ImageView)findViewById(R.id.v2333));
+        mapViews.put("2434", (ImageView)findViewById(R.id.v2434));
+        mapViews.put("3031", (ImageView)findViewById(R.id.h3031));
+        mapViews.put("3132", (ImageView)findViewById(R.id.h3132));
+        mapViews.put("3233", (ImageView)findViewById(R.id.h3233));
+        mapViews.put("3334", (ImageView)findViewById(R.id.h3334));
+        mapViews.put("3040", (ImageView)findViewById(R.id.v3040));
+        mapViews.put("3141", (ImageView)findViewById(R.id.v3141));
+        mapViews.put("3242", (ImageView)findViewById(R.id.v3242));
+        mapViews.put("3343", (ImageView)findViewById(R.id.v3343));
+        mapViews.put("3444", (ImageView)findViewById(R.id.v3444));
+        mapViews.put("4041", (ImageView)findViewById(R.id.h4041));
+        mapViews.put("4142", (ImageView)findViewById(R.id.h4142));
+        mapViews.put("4243", (ImageView)findViewById(R.id.h4243));
+        mapViews.put("4344", (ImageView)findViewById(R.id.h4344));
         
     }
 
