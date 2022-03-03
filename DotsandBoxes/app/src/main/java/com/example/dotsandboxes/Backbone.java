b886d0ea80e166b39b0baf460d63c159455681de
lopezboje <98184776+lopezboje@users.noreply.github.com>
2022-03-02 23:24:32 -0500

    MVP
    Able to update points, add lines (missing hashmap error), and close game when finished.
    
    Debug dotsConnected

--- DotsandBoxes/app/src/main/java/com/example/dotsandboxes/Backbone.java
+++ DotsandBoxes/app/src/main/java/com/example/dotsandboxes/Backbone.java
@@ -132,7 +132,7 @@ public class Backbone {
     }
     //lets program know if a box was filled in
 
-    public void dotsConnected(int x1, int x2, int y1, int y2){
+    public void dotsConnected(int x1, int y1, int x2, int y2){
         this.x1 = x1;
         this.x2 = x2;
         this.y1 = y1;
--- DotsandBoxes/app/src/main/java/com/example/dotsandboxes/Play.java
+++ DotsandBoxes/app/src/main/java/com/example/dotsandboxes/Play.java
@@ -23,6 +23,7 @@ public class Play extends AppCompatActivity {
         setContentView(R.layout.activity_play);
 
         back.clearBoard();
+        mapID();
     }
 
     public void dotClicked(View view){
@@ -35,14 +36,20 @@ public class Play extends AppCompatActivity {
 
 
             back.dotsConnected(coords.get(0), coords.get(1), coords.get(2), coords.get(3));
+            System.out.println(back.x1 + " " + coords.get(0));
+            System.out.println(back.y1 + " " + coords.get(1));
+            System.out.println(back.x2 + " " + coords.get(2));
+            System.out.println(back.y2 + " " + coords.get(3));
 
             String location = "";
+
             if(back.x1 == back.x2)
-                location += back.x1 + Math.min(back.y1, back.y2) + back.x2 + Math.max(back.y1, back.y2);
+                location += back.x1 + "" + Math.min(back.y1, back.y2) + "" + back.x2 + "" + Math.max(back.y1, back.y2);
             else if(back.y1 == back.y2)
-                location += Math.min(back.x1, back.x2) + back.y1 + Math.max(back.x1, back.x2) + back.y2;
+                location += Math.min(back.x1, back.x2) + "" + back.y1 + "" + Math.max(back.x1, back.x2) + "" + back.y2;
 
             mapViews.get(location).setVisibility(View.VISIBLE);
+
 
             if(!back.isConnectPoints(back.x1, back.x2, back.y1, back.y2)){
                 back.currTurn = !back.currTurn;
@@ -57,11 +64,17 @@ public class Play extends AppCompatActivity {
                 temp.setText("Current Turn: Blue");
             }
 
+            temp = (TextView) findViewById(R.id.scoreRedText);
+            temp.setText("Red: " + back.p1points);
+            temp = (TextView) findViewById(R.id.scoreBlueText);
+            temp.setText("Blue: " + back.p2points);
+
             buttonClicked = 0;
             coords.clear();
             back.numMoves--;
-
+            System.out.println(back.numMoves);
             if(back.numMoves == 0) {
+                finish();
                 Intent intent = new Intent(Play.this, MainActivity.class);
                 startActivity(intent);
             }
@@ -73,10 +86,42 @@ public class Play extends AppCompatActivity {
         mapViews.put("0102", (ImageView)findViewById(R.id.h0102));
         mapViews.put("0203", (ImageView)findViewById(R.id.h0203));
         mapViews.put("0304", (ImageView)findViewById(R.id.h0304));
-        
-    }
-
-    public void resetUI(){
+        mapViews.put("0010", (ImageView)findViewById(R.id.v0010));
+        mapViews.put("0111", (ImageView)findViewById(R.id.v0111));
+        mapViews.put("0212", (ImageView)findViewById(R.id.v0212));
+        mapViews.put("0313", (ImageView)findViewById(R.id.v0313));
+        mapViews.put("0414", (ImageView)findViewById(R.id.v0414));
+        mapViews.put("1011", (ImageView)findViewById(R.id.h1011));
+        mapViews.put("1112", (ImageView)findViewById(R.id.h1112));
+        mapViews.put("1213", (ImageView)findViewById(R.id.h1213));
+        mapViews.put("1314", (ImageView)findViewById(R.id.h1314));
+        mapViews.put("1020", (ImageView)findViewById(R.id.v1020));
+        mapViews.put("1121", (ImageView)findViewById(R.id.v1121));
+        mapViews.put("1222", (ImageView)findViewById(R.id.v1222));
+        mapViews.put("1323", (ImageView)findViewById(R.id.v1323));
+        mapViews.put("1424", (ImageView)findViewById(R.id.v1424));
+        mapViews.put("2021", (ImageView)findViewById(R.id.h2021));
+        mapViews.put("2122", (ImageView)findViewById(R.id.h2122));
+        mapViews.put("2223", (ImageView)findViewById(R.id.h2223));
+        mapViews.put("2324", (ImageView)findViewById(R.id.h2324));
+        mapViews.put("2030", (ImageView)findViewById(R.id.v2030));
+        mapViews.put("2131", (ImageView)findViewById(R.id.v2131));
+        mapViews.put("2232", (ImageView)findViewById(R.id.v2232));
+        mapViews.put("2333", (ImageView)findViewById(R.id.v2333));
+        mapViews.put("2434", (ImageView)findViewById(R.id.v2434));
+        mapViews.put("3031", (ImageView)findViewById(R.id.h3031));
+        mapViews.put("3132", (ImageView)findViewById(R.id.h3132));
+        mapViews.put("3233", (ImageView)findViewById(R.id.h3233));
+        mapViews.put("3334", (ImageView)findViewById(R.id.h3334));
+        mapViews.put("3040", (ImageView)findViewById(R.id.v3040));
+        mapViews.put("3141", (ImageView)findViewById(R.id.v3141));
+        mapViews.put("3242", (ImageView)findViewById(R.id.v3242));
+        mapViews.put("3343", (ImageView)findViewById(R.id.v3343));
+        mapViews.put("3444", (ImageView)findViewById(R.id.v3444));
+        mapViews.put("4041", (ImageView)findViewById(R.id.h4041));
+        mapViews.put("4142", (ImageView)findViewById(R.id.h4142));
+        mapViews.put("4243", (ImageView)findViewById(R.id.h4243));
+        mapViews.put("4344", (ImageView)findViewById(R.id.h4344));
 
     }
 }
